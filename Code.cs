f9cfd4d271184ce702c9e10dd3d3467ac0a50f6d
leevg <leevg@yandex.ru>
2022-05-27 16:07:22 +0300

    Inject article service

--- Code.cs
+++ Code.cs
@@ -1,6 +1,10 @@
 public class TestService{
+	
 	private readonly IAuthService _authService;
-	public TestService(IAuthService authService){
+	private readonly IArticleService _articleService;
+	
+	public TestService(IAuthService authService, IArticleService articleService){
 		_authService = authService;
+		_articleService = articleService;
 	}
 }
