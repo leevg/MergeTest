b55c9732c26a783fe04dcfebde87c3d3581d687b
OlehHorchyn <101092941+HorchynOleh@users.noreply.github.com>
2022-05-31 12:09:49 +0300

    Added ArticleService

--- Code.cs
+++ Code.cs
@@ -2,6 +2,7 @@ public class TestService{
 	
 	private readonly IAuthService _authService;
 	private readonly IAdvisorService _advisorService;
+	private readonly IArticleService _articleService;
 	public TestService(IAuthService authService, IAdvisorService advisorService, IArticleService article){
 		_authService = authService;
 		_advisorService = advisorService;
