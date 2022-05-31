01185ebec6d3fc4ba074d511d4286dd3a7845bc6
OlehHorchyn <101092941+HorchynOleh@users.noreply.github.com>
2022-05-31 12:15:03 +0300

    Added news service

--- Code.cs
+++ Code.cs
@@ -3,9 +3,12 @@ public class TestService{
 	private readonly IAuthService _authService;
 	private readonly IAdvisorService _advisorService;
 	private readonly IArticleService _articleService;
-	public TestService(IAuthService authService, IAdvisorService advisorService, IArticleService article){
+	private readonly INewsService _newsServ;
+	
+	public TestService(IAuthService authService, IAdvisorService advisorService, IArticleService articleService, INewsService newsServ){
 		_authService = authService;
 		_advisorService = advisorService;
 		_articleService = articleService;
+		_newServ = newsServ;
 	}
 }
