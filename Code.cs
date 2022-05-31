c65552c4a8504650549a550599d1ddebc77385b0
OlehHorchyn <101092941+HorchynOleh@users.noreply.github.com>
2022-05-31 12:18:26 +0300

    Fix

--- Code.cs
+++ Code.cs
@@ -3,12 +3,12 @@ public class TestService{
 	private readonly IAuthService _authService;
 	private readonly IAdvisorService _advisorService;
 	private readonly IArticleService _articleService;
-	private readonly INewsService _newsServ;
+	private readonly INewsService _newsService;
 	
-	public TestService(IAuthService authService, IAdvisorService advisorService, IArticleService articleService, INewsService newsServ){
+	public TestService(IAuthService authService, IAdvisorService advisorService, IArticleService articleService, INewsService newsService){
 		_authService = authService;
 		_advisorService = advisorService;
 		_articleService = articleService;
-		_newServ = newsServ;
+		_newService = newsService;
 	}
 }
